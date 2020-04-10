ca9f4b94be9d9ab7c7285f36d97102bcea4bbfeb
Barney Gammond <barney@gammond.co.uk>
2020-04-10 19:46:16 +0100

    Added api routes so that it will work with the front-end app

--- src/routes/rest-routes.ts
+++ src/routes/rest-routes.ts
@@ -5,8 +5,8 @@ import controller = require('controllers');
 export const restRouter = new Router();
 
 //Routes for the user entity
-restRouter.get('/players', controller.players.getPlayers);             //Get all users in the database
-restRouter.get('/players/:id', controller.players.getPlayer);
-restRouter.post('/players', controller.players.createPlayer);
-restRouter.put('/players/:id', controller.players.updatePlayer);
-restRouter.delete('/players/:id', controller.players.deletePlayer);
+restRouter.get('/api/players', controller.players.getPlayers);             //Get all users in the database
+restRouter.get('/api/players/:id', controller.players.getPlayer);
+restRouter.post('/api/players', controller.players.createPlayer);
+restRouter.put('/api/players/:id', controller.players.updatePlayer);
+restRouter.delete('/api/players/:id', controller.players.deletePlayer);
